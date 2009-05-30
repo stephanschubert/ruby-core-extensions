4447a9327c7a64f201fdce0114fa8cd87c26c44c
jazen <stephan@frozencherry.de>
2009-05-30 19:35:34 +0200

    Added File#replace - simple replacement of patterns in a file.

---
+++ lib/core_extensions/file.rb
@@ -0,0 +1,34 @@
+module CoreExtensions
+  module File
+
+    ##
+    # Replaces all occurrences of %pattern with %string.
+    # Works per line currently. Returns true if there
+    # were any replacements and false otherwise.
+    #
+    def replace(pattern, string)
+      full_path = File.expand_path(path)
+
+      unless File.file?(full_path)
+        raise ArgumentError, "#{path} is not a file."
+      end
+
+      reopen(full_path, "r")
+      lines = readlines
+
+      changed = false
+      lines.each { |line|
+        changed = true if line.gsub!(pattern, string)
+      }
+
+      if changed
+        reopen(full_path, "w")
+        lines.each { |line| write(line) }
+        close
+      end
+
+      changed
+    end
+    
+  end
+end
