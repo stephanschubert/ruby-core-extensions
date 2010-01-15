f6ae0dd4edc8308cdfda13c9308644da00d22035
jazen <stephan@frozencherry.de>
2010-01-15 21:13:07 +0100

    [BUGFIX] Use splat operator to ensure it works even when asking for a single key.

--- lib/core_extensions/hash.rb
+++ lib/core_extensions/hash.rb
@@ -16,13 +16,13 @@ module CoreExtensions
     # >> { :a => 1, :b => 2, :c => 3 }.pick(:a, :c)
     # => [ 1, 3 ]
     def pick(*keys)
-      keys.map { |k| self[k] }
+      return *keys.map { |k| self[k] }
     end
 
     # Same return value as #pick but deletes the
     # the keys from the hash also.
     def pluck(*keys)
-      keys.map { |k| delete(k) }
+      return *keys.map { |k| delete(k) }
     end
     
   end
